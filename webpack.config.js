14e9b842f67fe634b0bdbc36e299030a46a46cd3
Bartosz Krajka <krajka.bartosz@gmail.com>
2017-09-27 20:48:24 +0200

    Connect to backend - populate invoice index table
    * jQuery is used in AJAX - to be replaced by Angular anyway

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-
 const extractSass = new ExtractTextPlugin({filename: "css/[name].css", allChunks: true});
 
 module.exports = {
